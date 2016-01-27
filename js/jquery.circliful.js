c698f4f01d27164d591c63e20384f644e3bbb73e
Michael Thorne <michael.thorne@userx.co.za>
2016-01-27 12:01:26 +0200

    Added ability to configure `CanvasRenderingContext2D.lineCap`

--- js/jquery.circliful.js
+++ js/jquery.circliful.js
@@ -20,12 +20,13 @@
             border: 'default',
             complete: null,
             bordersize: 10,
-            delay: 0
+            delay: 0,
+            linecap: 'butt'
         }, options);
 
         return this.each(function () {
 
-            var customSettings = ["fgcolor", "bgcolor", "fill", "width", "dimension", "fontsize", "animationstep", "endPercent", "icon", "iconcolor", "iconsize", "border", "startdegree", "bordersize", "delay"];
+            var customSettings = ["fgcolor", "bgcolor", "fill", "width", "dimension", "fontsize", "animationstep", "endPercent", "icon", "iconcolor", "iconsize", "border", "startdegree", "bordersize", "delay", "linecap"];
 
             var customSettingsObj = {};
             var icon = '';
@@ -246,6 +247,7 @@
                     context.lineWidth = customSettingsObj.width - customSettingsObj.bordersize;
                 }
 
+                context.lineCap = customSettingsObj.linecap;
                 context.strokeStyle = customSettingsObj.fgcolor;
                 context.stroke();
 
