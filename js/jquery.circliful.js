32bec7702bb5f6e9bd3e9486183ce73842d7e2e0
philippsengelmann <philipp.sengelmann@dc-development.de>
2018-07-24 13:19:01 +0200

    epmty the icon-string variable if no icon is specified, to supress "undefined" strings in SVG-Output.

--- js/jquery.circliful.js
+++ js/jquery.circliful.js
@@ -133,8 +133,10 @@
 
             if (settings.icon !== 'none') {
                 icon = '<text text-anchor="middle" x="' + iconX + '" y="' + iconY + '" class="icon" style="font-size: ' + settings.iconSize + 'px" fill="' + settings.iconColor + '">&#x' + settings.icon + '</text>';
+            } else {
+                icon = '';
             }
-
+            
             if (settings.halfCircle) {
                 var rotate = 'transform="rotate(-180,100,100)"';
                 circleContainer
